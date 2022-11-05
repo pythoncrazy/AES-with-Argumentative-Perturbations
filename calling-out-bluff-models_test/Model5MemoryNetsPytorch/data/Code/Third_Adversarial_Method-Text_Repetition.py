b68d3cfc553d59d1152abd95cd267931819ef652
que-encrypt <37413746+pythoncrazy@users.noreply.github.com>
2022-11-05 01:28:30 -0700

    Added more methods

---
+++ calling-out-bluff-models_test/Model5MemoryNetsPytorch/data/Code/Third_Adversarial_Method-Text_Repetition.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from itertools import chain
+
+import re
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    if "..." in text: text = text.replace("...","<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+
+
+df = pd.read_csv("C:\\Users\\viksp\\Documents\\Folder_of_Folders\\AES-with-Argumentative-Perturbations\\calling-out-bluff-models_test\\Model5MemoryNetsPytorch\\data\\2nd_method\\dev_set_1_essays.tsv", sep='\t',encoding="utf-8")
+essayColumn = df[["essay"]].values
+temp_essays = []
+
+
+
+for essay in essayColumn:
+    essay=essay[0]
+    sentences = split_into_sentences(essay)
+
+    whole_essay = " ".join(sentences[0:len(sentences)//2+1]) + " ".join(sentences[len(sentences)//2+1:])
+    temp_essays.append(whole_essay)
+    
+
+print(len(temp_essays))
+
+i=0
+
+for essay in essayColumn:
+    essay=list(temp_essays[i])
+    i+=1
+
+df.to_csv('dev_2nd_method_set_1.tsv', sep="\t",index=False)
+
